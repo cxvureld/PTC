c117a45b94badb1bf1967c32902a080989839680
cxvureld <mishapak1404@gmail.com>
2024-12-12 05:35:41 +0700

    Reposit

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -6,13 +6,13 @@ const Modal = ({ isOpen, onClose, children }) => {
     
   useEffect(() => {
     if (isOpen) {
-      document.body.style.overflow = 'hidden'; // Отключаем прокрутку
+      document.body.style.overflow = 'hidden'; 
     } else {
-      document.body.style.overflow = 'unset'; // Включаем прокрутку обратно
+      document.body.style.overflow = 'unset'; 
     }
 
     return () => {
-      document.body.style.overflow = 'unset'; // Убираем стиль при размонтировании
+      document.body.style.overflow = 'unset'; 
     };
   }, [isOpen]);
 
